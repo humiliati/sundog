ee51278d77fdee3ebaa9490442d2245a45c99e0e
humiliati <ramoneez@yahoo.com>
2025-05-25 18:22:10 -0700

    proven

--- env.py
+++ env.py
@@ -12,11 +12,19 @@ class SundogEnv:
 
     def reset(self):
         mujoco.mj_resetData(self.model, self.data)
+        self.data.qpos[:] += np.random.uniform(-0.01, 0.01, size=self.data.qpos.shape)
+        mujoco.mj_forward(self.model, self.data)
+        target = self.data.site("laser_dot").xpos
+        tip = self.data.site("tip").xpos
+        offset = target - tip
+        self.data.qpos[6] += offset[1] * 0.05
+        self.data.qpos[7] += offset[2] * 0.05
+        self.data.qpos[6] += 0.02
         return self._get_obs()
 
     def step(self, ctrl):
         ctrl = np.clip(ctrl, -1, 1)
-        self.data.ctrl[:] = ctrl[-2:]  # Trim to match pitch1 and pitch2 actuators
+        self.data.ctrl[:] = ctrl[-2:]  # pitch1 and pitch2
         mujoco.mj_step(self.model, self.data)
         return self._get_obs(), self._compute_reward(), False, {}
 
@@ -25,11 +33,13 @@ class SundogEnv:
         laser_pos = self.data.site("laser_dot").xpos.copy()
         torque = self.data.qfrc_actuator.copy()
         shadow_pos = estimate_shadow(tip_pos, self.shadow_angle)
+        spread = np.linalg.norm(tip_pos[:2] - laser_pos[:2])
         return {
             "tip_pos": tip_pos,
             "laser_pos": laser_pos,
             "torque": torque,
-            "shadow_pos": shadow_pos
+            "shadow_pos": shadow_pos,
+            "bloom_spread": spread
         }
 
     def _compute_reward(self):
